6ddb60341233a296efabbd0e0ec865a6277c9931
kevin-gui <kevinguimail@gmail.com>
2024-07-15 17:11:29 -0400

    no file change work here

--- R/render_national.R
+++ R/render_national.R
@@ -31,8 +31,8 @@ render_national <- function(df,country_name,year,output_dir){
 
   render_quarto_document(df=df,country_name = country_name, year = year, parameterized = FALSE)
 
-  output_dir <- fs::dir_create(output_dir)
-
-  move_rendered_files(file_name, output_dir)
+  # output_dir <- fs::dir_create(output_dir)
+  #
+  # move_rendered_files(output_dir)
 
 }
--- R/utils_move_rendered_files.R
+++ R/utils_move_rendered_files.R
@@ -5,13 +5,9 @@
 #' @param file_name The base name for the output files
 #' @param output_dir The output directory
 
-move_rendered_files <- function(file_name, output_dir) {
-  files <- fs::dir_ls(here::here("quarto"), regexp = ".html$|.docx$|.pdf$")
-  new_file_names <- sapply(files, function(file) rename_file(file, file_name))
+move_rendered_files <- function(output_dir) {
 
-  for (i in seq_along(files)) {
-    old_path <- files[i]
-    new_path <- fs::path(output_dir, new_file_names[i])
-    fs::file_move(old_path, new_path)
-  }
+  cur_file <- fs::dir_ls(here::here("quarto"), regexp = ".html$|.docx$|.pdf$")
+  fs::file_move(cur_file, output_dir)
+
 }
